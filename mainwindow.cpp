783b67b5e9b1d3dd40f4678496e32f552bda5c93
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-02-21 22:06:53 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,1 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QSerialPort>
+#include <QSerialPortInfo>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
 
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QSerialPort serialPort;
+void MainWindow::on_dial_sliderMoved(int position)
+{
+serialPort.setPortName("COM4");
+serialPort.setBaudRate(QSerialPort::Baud9600);
+if (serialPort.open(QIODevice::ReadWrite))
+{
+    serialPort.write(QByteArray::number(position));
+}
+}
